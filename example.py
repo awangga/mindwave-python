5548c2e7f407f61c2318f6646d0528565db78099
Rolly Maulana Awangga <rolly@awang.ga>
2019-03-25 12:10:41 +0700

    add exapmple of getting raw

---
+++ example.py
@@ -0,0 +1,19 @@
+import mindwave,time
+
+port="COM4"
+mid="1425"
+
+headset = mindwave.Headset(port,mid)
+
+headset.connect()
+print "Connecting"
+
+while headset.status != "connected":
+	time.sleep(0.5)
+	if headset.status == "standby":
+		headset.connect()
+		print "Retrying connect..."
+print "Connected."
+
+While True:
+	print headset.raw_value
