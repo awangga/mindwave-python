cb634146b35807a0ef0b64e5bc4e47207ae96f37
Rolly Maulana Awangga <rolly@awang.ga>
2019-03-25 13:10:37 +0700

    adding example

--- example.py
+++ example.py
@@ -7,15 +7,16 @@ rate=0.001953125
 headset = mindwave.Headset(port,mid)
 
 headset.connect()
-print "Connecting"
+print("Connecting")
 
 while headset.status != "connected":
 	time.sleep(0.5)
 	if headset.status == "standby":
 		headset.connect()
-		print "Retrying connect..."
-print "Connected."
+		print("Retrying connect...")
+print("Connected.")
 
-While True:
-	print headset.raw_value
+
+while True:
+	print(headset.raw_value)
 	time.sleep(rate)
